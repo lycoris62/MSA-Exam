de3f52f404cebab6548470bbf3c93add240ae1eb
barami62 <wodbs7758@naver.com>
2024-08-07 18:05:34 +0900

    Feat(product): 상품 조회 요청 DTO 추가

---
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/dto/request/GetProductReq.java
@@ -0,0 +1,11 @@
+package com.sparta.msa_exam.product.domain.product.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * DTO 규칙으로 요청이면 -Req 접미사를 사용하며, 레코드를 사용하여 불변을 보장합니다.
+ * API 당 필수적으로 독립적인 DTO 포함하도록 하였습니다.
+ */
+@JsonIgnoreProperties // 필드가 없을 시 직렬화 에러가 나기 때문에 추가 필요
+public record GetProductReq() {
+}
