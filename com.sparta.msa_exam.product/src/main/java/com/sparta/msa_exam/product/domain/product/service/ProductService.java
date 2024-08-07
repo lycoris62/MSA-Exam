894f43bd1d76da8369acf9e9d176de2aaf73dc4b
barami62 <wodbs7758@naver.com>
2024-08-07 18:06:26 +0900

    Feat(product): 상품 조회 서비스 로직 추가

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/service/ProductService.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.sparta.msa_exam.product.domain.product.service;
 
 import com.sparta.msa_exam.product.domain.product.dto.request.AddProductReq;
+import com.sparta.msa_exam.product.domain.product.dto.request.GetProductReq;
 import com.sparta.msa_exam.product.domain.product.dto.response.AddProductRes;
+import com.sparta.msa_exam.product.domain.product.dto.response.GetProductRes;
 import com.sparta.msa_exam.product.domain.product.entity.Product;
 import com.sparta.msa_exam.product.domain.product.mapper.ProductMapper;
 import com.sparta.msa_exam.product.domain.product.repository.ProductRepository;
@@ -10,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -17,6 +21,15 @@ public class ProductService {
 
     private final ProductMapper mapper;
     private final ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public List<GetProductRes> getProductList(GetProductReq request) {
+
+        return productRepository.findAll()
+                .stream()
+                .map(mapper::toGetProductRes)
+                .toList();
+    }
 
     @Transactional
     public AddProductRes addProduct(AddProductReq request) {
