94ff3d5db364a6122f16c0f542552f07e92cdd9f
barami62 <wodbs7758@naver.com>
2024-08-07 21:24:08 +0900

    Feat(product): 페이지네이션 서비스 로직 추가

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/service/ProductService.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/service/ProductService.java
@@ -9,6 +9,7 @@ import com.sparta.msa_exam.product.domain.product.mapper.ProductMapper;
 import com.sparta.msa_exam.product.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,10 +23,9 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     @Transactional(readOnly = true)
-    public List<GetProductsRes> getProductList(GetProductsReq request) {
+    public List<GetProductsRes> getProductList(GetProductsReq request, Pageable pageable) {
 
-        return productRepository.findAll()
-                .stream()
+        return productRepository.findAllBy(pageable)
                 .map(ProductMapper.INSTANCE::toGetProductRes)
                 .toList();
     }
