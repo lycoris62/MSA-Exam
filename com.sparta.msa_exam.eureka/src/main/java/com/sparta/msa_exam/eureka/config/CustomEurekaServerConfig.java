daaf5cbab41875c87c8cb90394fbdee1d996f6b6
barami62 <wodbs7758@naver.com>
2024-08-07 13:20:51 +0900

    Docs(eureka): 유레카 설정 클래스 명명 이유 작성

--- com.sparta.msa_exam.eureka/src/main/java/com/sparta/msa_exam/eureka/config/CustomEurekaServerConfig.java
+++ com.sparta.msa_exam.eureka/src/main/java/com/sparta/msa_exam/eureka/config/CustomEurekaServerConfig.java
@@ -3,6 +3,11 @@ package com.sparta.msa_exam.eureka.config;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 유레카 설정 클래스를 따로 뺀 이유와 명명법은 아래 링크에서 확인하실 수 있습니다.
+ *
+ * @see <a href="https://promisingmoon.tistory.com/193">EurekaServerConfig 빈 중복 해결하기</a>
+ */
 @Configuration
 @EnableEurekaServer
 public class CustomEurekaServerConfig {
