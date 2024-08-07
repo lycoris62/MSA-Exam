bd624c8e5d1386faef5d6b483b807b8fe38bcc59
barami62 <wodbs7758@naver.com>
2024-08-07 21:23:53 +0900

    Feat(product): 오프셋 페이지네이션 추가

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/repository/ProductRepository.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.sparta.msa_exam.product.domain.product.repository;
 
 import com.sparta.msa_exam.product.domain.product.entity.Product;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Slice<Product> findAllBy(Pageable pageable);
 }
