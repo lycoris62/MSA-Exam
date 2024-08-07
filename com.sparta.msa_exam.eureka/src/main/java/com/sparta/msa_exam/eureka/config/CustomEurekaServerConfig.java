4556cddabaac302135d8c1c17d2659697b92ad66
barami62 <wodbs7758@naver.com>
2024-08-07 13:13:44 +0900

    Feat(eureka): 유레카 서버 설정 클래스 추가

---
+++ com.sparta.msa_exam.eureka/src/main/java/com/sparta/msa_exam/eureka/config/CustomEurekaServerConfig.java
@@ -0,0 +1,9 @@
+package com.sparta.msa_exam.eureka.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class CustomEurekaServerConfig {
+}
