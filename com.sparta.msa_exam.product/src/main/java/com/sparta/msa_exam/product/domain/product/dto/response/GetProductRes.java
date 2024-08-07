8c918739ce2fd6c3472720d11c9603866ec49897
barami62 <wodbs7758@naver.com>
2024-08-07 20:19:03 +0900

    Docs(product): 상품 목록 조회 DTO 스웨거 적용

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/dto/response/GetProductRes.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/dto/response/GetProductRes.java
@@ -1,12 +1,21 @@
 package com.sparta.msa_exam.product.domain.product.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * DTO 규칙으로 응답이면 -Res 접미사를 사용하며, 레코드를 사용하여 불변을 보장합니다.
  * API 당 필수적으로 독립적인 DTO 포함하도록 하였습니다.
  */
+@Schema(title = "상품 조회 응답 DTO")
 public record GetProductRes(
+
+        @Schema(description = "상품 아이디", example = "1")
         Long productId,
+
+        @Schema(description = "상품 이름", example = "apple")
         String name,
+
+        @Schema(description = "공급가", example = "1000")
         Integer supplyPrice
 ) {
 }
