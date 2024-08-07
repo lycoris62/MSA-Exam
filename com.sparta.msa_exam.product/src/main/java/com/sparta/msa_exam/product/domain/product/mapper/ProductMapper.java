684b5fa2aeed44a72d251584ac1d64c2ddd6681c
barami62 <wodbs7758@naver.com>
2024-08-07 18:06:07 +0900

    Feat(product): 상품 조회 DTO 매퍼 추가

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/mapper/ProductMapper.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package com.sparta.msa_exam.product.domain.product.mapper;
 
 import com.sparta.msa_exam.product.domain.product.dto.response.AddProductRes;
+import com.sparta.msa_exam.product.domain.product.dto.response.GetProductRes;
 import com.sparta.msa_exam.product.domain.product.entity.Product;
 import org.mapstruct.Mapper;
 
@@ -8,6 +9,8 @@ import static org.mapstruct.MappingConstants.ComponentModel;
 
 @Mapper(componentModel = ComponentModel.SPRING)
 public interface ProductMapper {
+
+    GetProductRes toGetProductRes(Product product);
 
     AddProductRes toAddProductRes(Product product);
 }
