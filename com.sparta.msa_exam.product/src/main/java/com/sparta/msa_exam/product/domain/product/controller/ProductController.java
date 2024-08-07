2f1e9df129e29d5a98b192266ce3dec3a7a56e70
barami62 <wodbs7758@naver.com>
2024-08-07 18:06:58 +0900

    Feat(product): 상품 조회 컨트롤러 API 추가

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/controller/ProductController.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/controller/ProductController.java
@@ -1,15 +1,16 @@
 package com.sparta.msa_exam.product.domain.product.controller;
 
 import com.sparta.msa_exam.product.domain.product.dto.request.AddProductReq;
+import com.sparta.msa_exam.product.domain.product.dto.request.GetProductReq;
 import com.sparta.msa_exam.product.domain.product.dto.response.AddProductRes;
+import com.sparta.msa_exam.product.domain.product.dto.response.GetProductRes;
 import com.sparta.msa_exam.product.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -18,6 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<GetProductRes>> getProductList(GetProductReq request) {
+
+        List<GetProductRes> productList = productService.getProductList(request);
+
+        return ResponseEntity.ok(productList);
+    }
 
     @PostMapping
     public ResponseEntity<AddProductRes> addProduct(@RequestBody AddProductReq request) {
