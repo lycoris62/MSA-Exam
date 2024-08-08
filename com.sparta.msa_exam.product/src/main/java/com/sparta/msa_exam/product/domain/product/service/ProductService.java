08a0b280e1e03fbc81517d2f847037d69728e06a
barami62 <wodbs7758@naver.com>
2024-08-08 12:43:31 +0900

    Feat(product): 상품 서비스 로직에 캐싱 적용
    - 페이징에 따른 목록 조회 캐싱 적용
    - 상품 추가 시 캐싱 적용

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/service/ProductService.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/domain/product/service/ProductService.java
@@ -7,8 +7,12 @@ import com.sparta.msa_exam.product.domain.product.dto.response.GetProductsRes;
 import com.sparta.msa_exam.product.domain.product.entity.Product;
 import com.sparta.msa_exam.product.domain.product.mapper.ProductMapper;
 import com.sparta.msa_exam.product.domain.product.repository.ProductRepository;
+import com.sparta.msa_exam.product.global.redis.CacheName;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +27,10 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     @Transactional(readOnly = true)
+    @Cacheable(
+            cacheNames = CacheName.PRODUCT_ALL,
+            key = "{ #pageable.pageNumber, #pageable.pageSize }"
+    )
     public List<GetProductsRes> getProductList(GetProductsReq request, Pageable pageable) {
 
         return productRepository.findAllBy(pageable)
@@ -31,6 +39,8 @@ public class ProductService {
     }
 
     @Transactional
+    @CacheEvict(cacheNames = CacheName.PRODUCT_ALL, allEntries = true) // 목록 조회 캐싱 삭제
+    @CachePut(cacheNames = CacheName.PRODUCT, key = "#result.productId()") // 단일 캐싱 추가
     public AddProductRes addProduct(AddProductReq request) {
 
         Product product = Product.from(request);
