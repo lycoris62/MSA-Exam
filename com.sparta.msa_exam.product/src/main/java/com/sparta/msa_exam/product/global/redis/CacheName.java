c7b5a6bc1533911dab85fb9256b3cef1610a08e7
barami62 <wodbs7758@naver.com>
2024-08-08 12:40:24 +0900

    Feat(product): 캐시네임 상수 추가

---
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/global/redis/CacheName.java
@@ -0,0 +1,6 @@
+package com.sparta.msa_exam.product.global.redis;
+
+public class CacheName {
+    public static final String PRODUCT = "productCache";
+    public static final String PRODUCT_ALL = "productAllCache";
+}
