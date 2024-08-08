157cbc391f467d5ee4eb669c239e1a2e43f30ae5
barami62 <wodbs7758@naver.com>
2024-08-08 16:40:42 +0900

    Feat(order): 주문 추가 요청 DTO에 검증 로직 추가

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/dto/request/CreateOrderReq.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/dto/request/CreateOrderReq.java
@@ -1,9 +1,18 @@
 package com.sparta.msa_exam.order.domain.order.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.util.List;
 
 public record CreateOrderReq(
+
+        @NotBlank(message = "주문 이름은 공백이 될 수 없습니다.")
+        @Size(min = 1, max = 255, message = "주문의 이름은 1글자 이상, 255글자 이하 입니다.")
         String name,
+
+        @NotNull(message = "상품 아이디 리스트는 필수입니다.")
         List<Long> productIds
 ) {
 }
