137eeafdf237ab3e857eb2a594fd35e64bf27f99
barami62 <wodbs7758@naver.com>
2024-08-09 20:30:07 +0900

    Feat(order): 주문 아이템 추가 서비스 로직 추가

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/service/OrderService.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/service/OrderService.java
@@ -1,14 +1,18 @@
 package com.sparta.msa_exam.order.domain.order.service;
 
+import com.sparta.msa_exam.order.domain.order.dto.request.AddOrderItemReq;
 import com.sparta.msa_exam.order.domain.order.dto.request.CreateOrderReq;
+import com.sparta.msa_exam.order.domain.order.dto.response.AddOrderItemRes;
 import com.sparta.msa_exam.order.domain.order.dto.response.CreateOrderRes;
 import com.sparta.msa_exam.order.domain.order.entity.Order;
 import com.sparta.msa_exam.order.domain.order.mapper.OrderMapper;
 import com.sparta.msa_exam.order.domain.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @Service
@@ -25,5 +29,16 @@ public class OrderService {
         Order savedOrder = orderRepository.save(order);
 
         return orderMapper.toCreateOrderRes(savedOrder);
+    }
+
+    @Transactional
+    public AddOrderItemRes addOrderItem(Long orderId, AddOrderItemReq request) {
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        order.addOrderItem(request);
+
+        return new AddOrderItemRes(orderId, request.productId());
     }
 }
