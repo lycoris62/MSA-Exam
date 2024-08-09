d674f9ce5d38a5658eed649852ca1b4dd82017b2
barami62 <wodbs7758@naver.com>
2024-08-09 20:41:22 +0900

    Feat(order): 주문 단건 조회 서비스 로직 추가

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/service/OrderService.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/service/OrderService.java
@@ -4,6 +4,7 @@ import com.sparta.msa_exam.order.domain.order.dto.request.AddOrderItemReq;
 import com.sparta.msa_exam.order.domain.order.dto.request.CreateOrderReq;
 import com.sparta.msa_exam.order.domain.order.dto.response.AddOrderItemRes;
 import com.sparta.msa_exam.order.domain.order.dto.response.CreateOrderRes;
+import com.sparta.msa_exam.order.domain.order.dto.response.GetOrderRes;
 import com.sparta.msa_exam.order.domain.order.entity.Order;
 import com.sparta.msa_exam.order.domain.order.mapper.OrderMapper;
 import com.sparta.msa_exam.order.domain.order.repository.OrderRepository;
@@ -21,6 +22,15 @@ public class OrderService {
 
     private final OrderMapper orderMapper;
     private final OrderRepository orderRepository;
+
+    @Transactional(readOnly = true)
+    public GetOrderRes getOrder(Long orderId) {
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        return orderMapper.toGetOrderReq(order);
+    }
 
     @Transactional
     public CreateOrderRes createOrder(CreateOrderReq request) {
