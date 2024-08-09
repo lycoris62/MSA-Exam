de4334f256411aa637c3aa4b45f216515558e4dc
barami62 <wodbs7758@naver.com>
2024-08-09 20:30:58 +0900

    Feat(order): 주문 아이템 추가 API 추가

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/controller/OrderController.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.sparta.msa_exam.order.domain.order.controller;
 
+import com.sparta.msa_exam.order.domain.order.dto.request.AddOrderItemReq;
 import com.sparta.msa_exam.order.domain.order.dto.request.CreateOrderReq;
+import com.sparta.msa_exam.order.domain.order.dto.response.AddOrderItemRes;
 import com.sparta.msa_exam.order.domain.order.dto.response.CreateOrderRes;
 import com.sparta.msa_exam.order.domain.order.service.OrderService;
 import com.sparta.msa_exam.order.global.common.GlobalConstant;
@@ -9,10 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -29,6 +28,16 @@ public class OrderController {
     public ResponseEntity<CreateOrderRes> createOrder(@RequestBody @Valid CreateOrderReq request) {
 
         CreateOrderRes response = orderService.createOrder(request);
+
+        return ResponseEntity.ok()
+                .header(GlobalConstant.CUSTOM_SERVER_PORT_HEADER, port)
+                .body(response);
+    }
+
+    @PutMapping("/{orderId}")
+    public ResponseEntity<AddOrderItemRes> addOrderItem(@PathVariable Long orderId, @RequestBody @Valid AddOrderItemReq request) {
+
+        AddOrderItemRes response = orderService.addOrderItem(orderId, request);
 
         return ResponseEntity.ok()
                 .header(GlobalConstant.CUSTOM_SERVER_PORT_HEADER, port)
