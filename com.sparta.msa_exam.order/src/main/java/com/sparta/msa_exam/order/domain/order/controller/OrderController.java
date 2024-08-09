e454615ad666595c13a2f7e72073102849c9d643
barami62 <wodbs7758@naver.com>
2024-08-09 20:31:20 +0900

    Fix(order): order API 요구사항에 맞게 수정

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/controller/OrderController.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/controller/OrderController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/orders")
+@RequestMapping("/order")
 public class OrderController {
 
     private final OrderService orderService;
