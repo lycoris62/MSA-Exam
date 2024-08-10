6733e8e75d62a8511a2af17523516756faa4c676
barami62 <wodbs7758@naver.com>
2024-08-10 10:15:33 +0900

    Feat(auth): 로그인 DTO 추가

---
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/domain/auth/dto/request/SignInReq.java
@@ -0,0 +1,6 @@
+package com.sparta.msa_exam.auth.domain.auth.dto.request;
+
+public record SignInReq(
+        String user_id
+) {
+}
---
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/domain/auth/dto/response/SignInRes.java
@@ -0,0 +1,6 @@
+package com.sparta.msa_exam.auth.domain.auth.dto.response;
+
+public record SignInRes(
+        String accessToken
+) {
+}
