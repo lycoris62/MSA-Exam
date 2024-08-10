c0f2d3dd89de777040bff27d03cb4c47c306c219
barami62 <wodbs7758@naver.com>
2024-08-10 10:18:20 +0900

    Feat(auth): 유저 컨트롤러 추가
    - 로그인 API 추가

---
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/domain/auth/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.sparta.msa_exam.auth.domain.auth.controller;
+
+import com.sparta.msa_exam.auth.domain.auth.dto.request.SignInReq;
+import com.sparta.msa_exam.auth.domain.auth.dto.response.SignInRes;
+import com.sparta.msa_exam.auth.domain.auth.service.AuthService;
+import com.sparta.msa_exam.auth.global.common.GlobalConstant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/signIn")
+    public ResponseEntity<SignInRes> createAuthenticationToken(SignInReq request) {
+
+        SignInRes response = authService.signIn(request);
+
+        return ResponseEntity.ok()
+                .header(GlobalConstant.CUSTOM_SERVER_PORT_HEADER, port)
+                .body(response);
+    }
+}
