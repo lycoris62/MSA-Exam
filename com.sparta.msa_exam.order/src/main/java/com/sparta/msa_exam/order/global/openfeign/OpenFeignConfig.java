8d113e28f801911e69c913ef7209e002fa6356e9
barami62 <wodbs7758@naver.com>
2024-08-10 15:55:45 +0900

    Feat(order): OpenFeign 설정 추가

---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/global/openfeign/OpenFeignConfig.java
@@ -0,0 +1,9 @@
+package com.sparta.msa_exam.order.global.openfeign;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class OpenFeignConfig {
+}
