68e923b1d0d8340196e040ad6a44ca5ddfb61a5b
barami62 <wodbs7758@naver.com>
2024-08-10 15:13:48 +0900

    Feat(auth): 회원가입 API 추가

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/domain/auth/controller/AuthController.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/domain/auth/controller/AuthController.java
@@ -1,16 +1,17 @@
 package com.sparta.msa_exam.auth.domain.auth.controller;
 
 import com.sparta.msa_exam.auth.domain.auth.dto.request.SignInReq;
+import com.sparta.msa_exam.auth.domain.auth.dto.request.SignUpReq;
 import com.sparta.msa_exam.auth.domain.auth.dto.response.SignInRes;
+import com.sparta.msa_exam.auth.domain.auth.dto.response.SignUpRes;
 import com.sparta.msa_exam.auth.domain.auth.service.AuthService;
 import com.sparta.msa_exam.auth.global.common.GlobalConstant;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -27,6 +28,16 @@ public class AuthController {
     public ResponseEntity<SignInRes> createAuthenticationToken(SignInReq request) {
 
         SignInRes response = authService.signIn(request);
+
+        return ResponseEntity.ok()
+                .header(GlobalConstant.CUSTOM_SERVER_PORT_HEADER, port)
+                .body(response);
+    }
+
+    @PostMapping("/signUp")
+    public ResponseEntity<SignUpRes> signUp(@RequestBody @Valid SignUpReq request) {
+
+        SignUpRes response = authService.signUp(request);
 
         return ResponseEntity.ok()
                 .header(GlobalConstant.CUSTOM_SERVER_PORT_HEADER, port)
