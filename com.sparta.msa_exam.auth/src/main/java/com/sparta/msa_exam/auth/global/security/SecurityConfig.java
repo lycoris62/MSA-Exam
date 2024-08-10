cbd55b4c321209ad7e432db75f65b8022c045523
barami62 <wodbs7758@naver.com>
2024-08-10 10:18:59 +0900

    Feat(auth): 스프링 시큐리티 설정 추가

---
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/global/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.sparta.msa_exam.auth.global.security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.authorizeHttpRequests(authz -> authz
+                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                .requestMatchers("/auth/signIn").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+}
