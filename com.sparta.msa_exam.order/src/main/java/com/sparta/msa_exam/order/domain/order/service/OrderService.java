2ea4e26ca4b2dc75a73fd9b86ae2bf6f56027e9e
barami62 <wodbs7758@naver.com>
2024-08-12 11:49:18 +0900

    Feat(order): 주문 아이템 추가 시 상품 아이디 검증 추가

--- com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/service/OrderService.java
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/domain/order/service/OrderService.java
@@ -8,6 +8,8 @@ import com.sparta.msa_exam.order.domain.order.dto.response.GetOrderRes;
 import com.sparta.msa_exam.order.domain.order.entity.Order;
 import com.sparta.msa_exam.order.domain.order.mapper.OrderMapper;
 import com.sparta.msa_exam.order.domain.order.repository.OrderRepository;
+import com.sparta.msa_exam.order.global.openfeign.client.ProductClient;
+import com.sparta.msa_exam.order.global.openfeign.client.dto.response.GetProductsRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,23 +17,36 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderService {
 
     private final OrderMapper orderMapper;
+    private final ProductClient productClient;
     private final OrderRepository orderRepository;
 
+    /**
+     * 주문 단건 조회
+     */
     @Transactional(readOnly = true)
     public GetOrderRes getOrder(Long orderId) {
 
-        Order order = orderRepository.findById(orderId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Order order = findOrder(orderId);
 
         return orderMapper.toGetOrderReq(order);
     }
 
+    private Order findOrder(Long orderId) {
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    /**
+     * 주문 생성
+     */
     @Transactional
     public CreateOrderRes createOrder(CreateOrderReq request) {
 
@@ -41,14 +56,30 @@ public class OrderService {
         return orderMapper.toCreateOrderRes(savedOrder);
     }
 
+    /**
+     * 주문 아이템 추가
+     */
     @Transactional
     public AddOrderItemRes addOrderItem(Long orderId, AddOrderItemReq request) {
 
-        Order order = orderRepository.findById(orderId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Order order = findOrder(orderId);
 
+        // 상품 아이디를 담은 주문 아이템이 실제로 있는 상품인지를 검증
+        checkValidProductId(request);
+        // 주문에 주문 아이템 추가
         order.addOrderItem(request);
 
         return new AddOrderItemRes(orderId, request.productId());
+    }
+
+    private void checkValidProductId(AddOrderItemReq request) {
+
+        List<GetProductsRes> productInfoList = productClient.getProducts();
+
+        productInfoList.stream()
+                .map(GetProductsRes::productId)
+                .filter(productId -> productId.equals(request.productId()))
+                .findAny()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
