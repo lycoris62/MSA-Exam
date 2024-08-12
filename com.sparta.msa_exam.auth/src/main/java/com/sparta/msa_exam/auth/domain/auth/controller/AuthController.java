8d3626ee8b7f4fb63f6d8116b9ca7518c53f44ed
barami62 <wodbs7758@naver.com>
2024-08-12 14:58:24 +0900

    Fix(auth): 서버 포트 담는 로직 삭제

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/domain/auth/controller/AuthController.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/domain/auth/controller/AuthController.java
@@ -5,11 +5,9 @@ import com.sparta.msa_exam.auth.domain.auth.dto.request.SignUpReq;
 import com.sparta.msa_exam.auth.domain.auth.dto.response.SignInRes;
 import com.sparta.msa_exam.auth.domain.auth.dto.response.SignUpRes;
 import com.sparta.msa_exam.auth.domain.auth.service.AuthService;
-import com.sparta.msa_exam.auth.global.common.GlobalConstant;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,16 +19,12 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @Value("${server.port}")
-    private String port;
-
     @GetMapping("/signIn")
     public ResponseEntity<SignInRes> createAuthenticationToken(SignInReq request) {
 
         SignInRes response = authService.signIn(request);
 
         return ResponseEntity.ok()
-                .header(GlobalConstant.CUSTOM_SERVER_PORT_HEADER, port)
                 .body(response);
     }
 
@@ -40,7 +34,6 @@ public class AuthController {
         SignUpRes response = authService.signUp(request);
 
         return ResponseEntity.ok()
-                .header(GlobalConstant.CUSTOM_SERVER_PORT_HEADER, port)
                 .body(response);
     }
 }
--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/global/common/GlobalConstant.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.msa_exam.auth.global.common;
-
-public class GlobalConstant {
-
-    public static final String CUSTOM_SERVER_PORT_HEADER = "Server-Port";
-}
